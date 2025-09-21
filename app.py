5cfff4fe751d7a7d6e24deec60d132cf60205bb5
taemin2271 <taemin2271@gmail.com>
2025-09-21 18:24:53 +0900

    docs: AI 자세 교정 서비스 프로젝트 계획서 작성

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get("/")
-def index():
-    return "Hello, Flask!"
-
-@app.get("/home")
+@app.get("/hw1")
 def home():
-    return render_template("home.html", title="집에 가고싶다")
+    return render_template("hw1.html", title="웹캠 자세 교정 서비스 | 프로젝트 계획서")
 
 if __name__ == "__main__":
     app.run() # 기본 : http://127.0.0.1:5000
